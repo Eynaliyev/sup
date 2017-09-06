bd69d415f57e3535c46ebd5fa6292eb0a4e668e9
reynaliyev <reynaliyev@luxoft.com>
2017-09-06 20:31:56 +0200

    minor fixes

--- src/pages/meet-somebody/meet-somebody.ts
+++ src/pages/meet-somebody/meet-somebody.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-meet-somebody',
   templateUrl: 'meet-somebody.html'
 })
 export class MeetSomebodyPage {
+  private languages;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController) {
   }
-  
+
+  presentLoadingText() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: 'Loading Please Wait...'
+    });
+
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 5000);
+
+    loading.onDidDismiss(() => {
+      console.log('Dismissed loading');
+    });
+  }
 }

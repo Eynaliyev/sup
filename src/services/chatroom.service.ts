9adfb75e02a556a5943e1a8e316425ed42434301
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-10-18 23:28:28 +0200

    add; bunch of methods from FlyChats to chatroom service, prev commit: refactored providers into a separate file, added platform checking and mocking of native plugins

--- src/services/chatroom.service.ts
+++ src/services/chatroom.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http} from '@angular/http';
+//import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
@@ -19,6 +20,7 @@ export class ChatroomService {
 
 	constructor(
     private http: Http,
+//    public db: AngularFireDatabase,
     private utilService: UtilService
   )
   {
@@ -153,6 +155,65 @@ export class ChatroomService {
           observer.next(this.messages);
       });
     }
+    /*
+  // get a specific room from the list - get room reference
+  getRoom(uid: string, id: string): FirebaseObjectObservable<any> {
+    let roomRef = this.db.object(`/rooms/${uid},${id}`);
+    return roomRef;
+  }
+  findRoom():Promise<any>{
+    let res = new Promise<any>((resolve, reject) => resolve());
+    return res;
+  }
+  leaveRoom(){
+
+  }
+  getMessages(uid: string, id: string): FirebaseListObservable<any> {
+    let messagesRef = this.db.list(`/rooms/${uid},${id}/messages`);
+    return messagesRef;
+  }  // mapping to the room data used in-app
+  toRoom(userId, otherId, messages): Room{
+    // unique id based on the ids of two users
+    let room = {
+      id: userId,
+      messages: messages,
+      guysWaitlist: [],
+      girlsWaitlist: [],
+      location: [],
+      menNum: 0,
+      womenNum: 0,
+      members: []
+    }
+    return room;
+  }
+  // create a room - add room references to both users
+  addMessage(uid: string, otherId: string, message: Message): void {
+    //push it to the rooms list
+    let roomsRef = this.db.list(`/rooms/${uid},${otherId}/messages`);
+    if (message.picture === null){
+      roomsRef.push(message);
+    } else {
+      //assuming we have a picture so we'll need to store it in firebase
+      //storage and save the URL as a picture property
+      //generate a unique name for storing in firebase storage
+      let uidName = this.utilService.guid();
+      firebase.storage().ref(`/rooms/${uid},${otherId}/messages`)
+      .child(`${uidName}.png`)
+      .putString(message.picture, 'base64', {contentType: 'image/png'})
+      .then((savedPicture) => {
+        message.picture = savedPicture.downloadURL;
+        roomsRef.push(message);
+      });
+    }
+  }
+  // remove a room from the list
+  removeMessage(userId, otherId: string, messageId: string): void {
+    //rooms list
+    this.db.list(`/rooms/${userId},${otherId}/messages`).remove(messageId);
+  }
+
+
+    */
     getAvailableLanguages(): Observable<Language[]>{
       return new Observable(observer => {
         observer.next(this.languages);

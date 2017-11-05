bbc420b200abc0cb2bba0fdf2fc86ccdfaadc9ab
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-11-05 21:17:40 +0100

    fix: minor fixes

--- src/services/chatroom.service.ts
+++ src/services/chatroom.service.ts
@@ -13,6 +13,7 @@ import { UtilService } from '../shared/util.service';
 import {Language} from '../models/models';
 import {LANGUAGES} from './mock-languages';
 import {Participant} from '../models/models';
+
 @Injectable()
 export class ChatroomService {
   private messages: Message[];
@@ -115,14 +116,13 @@ export class ChatroomService {
 			};
 			console.log(user, participant);
 			chatroom[thisGender].push(participant);
-			console.log('creating chatroom: ', chatroom);
-			console.log(user);
+			console.log('creating chatroom: ', chatroom, user);
 			return this.db.object(`chatrooms/${randomId}`).set(chatroom);
 		}
     leaveChatroom(chatroomId: string, userId: string){
 			// remove the user from participants list
-			let participants = this.db.object(`chatrooms/${chatroomId}/participants/${userId}`);
-			participants.remove();
+			let participant = this.db.object(`chatrooms/${chatroomId}/participants/${userId}`);
+			participant.remove();
       // publish a 'left the room  type of message' - a cloud function?
       // remove the chatroom id from local storage
     }
@@ -139,65 +139,6 @@ export class ChatroomService {
     getMessages(chatroomId: string, start?, end?): Observable<any[]> {
       return this.db.list(`chatrooms/${chatroomId}/messages`).valueChanges();
     }
-    /*
-  // get a specific room from the list - get room reference
-  getRoom(uid: string, id: string): FirebaseObjectObservable<any> {
-    let roomRef = this.db.object(`/rooms/${uid},${id}`);
-    return roomRef;
-  }
-  findRoom():Promise<any>{
-    let res = new Promise<any>((resolve, reject) => resolve());
-    return res;
-  }
-  leaveRoom(){
-
-  }
-  getMessages(uid: string, id: string): FirebaseListObservable<any> {
-    let messagesRef = this.db.list(`/rooms/${uid},${id}/messages`);
-    return messagesRef;
-  }  // mapping to the room data used in-app
-  toRoom(userId, otherId, messages): Room{
-    // unique id based on the ids of two users
-    let room = {
-      id: userId,
-      messages: messages,
-      guysWaitlist: [],
-      girlsWaitlist: [],
-      location: [],
-      menNum: 0,
-      womenNum: 0,
-      members: []
-    }
-    return room;
-  }
-  // create a room - add room references to both users
-  addMessage(uid: string, otherId: string, message: Message): void {
-    //push it to the rooms list
-    let roomsRef = this.db.list(`/rooms/${uid},${otherId}/messages`);
-    if (message.picture === null){
-      roomsRef.push(message);
-    } else {
-      //assuming we have a picture so we'll need to store it in firebase
-      //storage and save the URL as a picture property
-      //generate a unique name for storing in firebase storage
-      let uidName = this.utilService.guid();
-      firebase.storage().ref(`/rooms/${uid},${otherId}/messages`)
-      .child(`${uidName}.png`)
-      .putString(message.picture, 'base64', {contentType: 'image/png'})
-      .then((savedPicture) => {
-        message.picture = savedPicture.downloadURL;
-        roomsRef.push(message);
-      });
-    }
-  }
-  // remove a room from the list
-  removeMessage(userId, otherId: string, messageId: string): void {
-    //rooms list
-    this.db.list(`/rooms/${userId},${otherId}/messages`).remove(messageId);
-  }
-
-
-    */
     getAvailableLanguages(): Observable<any[]>{
       return this.afs.collection(`languages`).valueChanges();
     }

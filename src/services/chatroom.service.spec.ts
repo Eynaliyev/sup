313b23489479e1cfee32d4eec23643ada233e397
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-12-07 22:18:18 +0100

    minor fixed

---
+++ src/services/chatroom.service.spec.ts
@@ -0,0 +1,16 @@
+import { ChatroomService } from './services';
+let chatroomService = null;
+describe('Chatroom Service', () => {
+	beforeEach(() => {
+		//chatroomService = new ChatroomService();
+	});
+
+    it('should do nothing', () => {
+
+        expect(true).toBeTruthy();
+				expect(true).toBeTruthy();
+        expect(1 + 1).toBe(2);
+        //expect(2 + 2).toBe(5); //this will fail
+    });
+
+});

f081ac6ebd79a6f8d8ef8b58c278a9b44939fcab
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2018-03-30 12:43:45 +0200

    update: vip page

--- src/pages/vip/vip.ts
+++ src/pages/vip/vip.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
-import { PurchasePage } from '../purchase/purchase';
-import {VipService} from '../../services/services';
-import {UtilService} from '../../shared/util.service';
-import { AlertController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { PurchasePage } from "../purchase/purchase";
+import { VipService } from "../../services/services";
+import { UtilService } from "../../shared/util.service";
+import { AlertController } from "ionic-angular";
 
 /**
  * Generated class for the VIP page.
@@ -12,33 +12,31 @@ import { AlertController } from 'ionic-angular';
  * on Ionic pages and navigation.
  */
 @Component({
-  selector: 'page-vip',
-  templateUrl: 'vip.html',
+	selector: "page-vip",
+	templateUrl: "vip.html"
 })
 export class VIPPage {
-  constructor(
-    public navCtrl: NavController,
+	constructor(
+		public navCtrl: NavController,
 		public vipSrvc: VipService,
 		private alertCtrl: AlertController,
 		private utilSrvc: UtilService
-	 ) {
-  }
+	) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VIPPage');
-  }
-  watchAd(){
-		this.utilSrvc.presentFakedoorAlert('watchAd');
-    //this.vipSrvc.presentAd();
-  }
-  invite(){
-		this.utilSrvc.presentFakedoorAlert('invite');
-    //this.vipSrvc.inviteFriend();
-    //this.facebook.appInvite({applink, photoUrl});
-  }
-  viewPurchasePage(){
-		this.utilSrvc.presentFakedoorAlert('viewPurchasePage');
-    //this.navCtrl.push(PurchasePage);
+	ionViewDidLoad() {
+		console.log("ionViewDidLoad VIPPage");
 	}
-
+	watchAd() {
+		this.utilSrvc.presentFakedoorAlert("watchAd");
+		//this.vipSrvc.presentAd();
+	}
+	invite() {
+		this.utilSrvc.presentFakedoorAlert("invite");
+		//this.vipSrvc.inviteFriend();
+		//this.facebook.appInvite({applink, photoUrl});
+	}
+	viewPurchasePage() {
+		this.utilSrvc.presentFakedoorAlert("viewPurchasePage");
+		//this.navCtrl.push(PurchasePage);
+	}
 }

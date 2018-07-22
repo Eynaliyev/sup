c92386fb7be12e9421202fa7acf7608d61af2c16
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2018-07-22 18:29:29 +0200

    fix: default service

--- src/shared/default-image.directive.ts
+++ src/shared/default-image.directive.ts
@@ -1,17 +1,20 @@
-import {Directive, Input} from '@angular/core'
+import { Directive, Input } from "@angular/core";
 
 @Directive({
-  selector: 'img[default]',
-  host: {
-    '(error)':'updateUrl()',
-    '[src]':'src'
-   }
+	selector: "img[default]",
+	host: {
+		"[src]": "checkPath(src)",
+		"(error)": "updateUrl()"
+	}
 })
 export class DefaultImageDirective {
-  @Input() src:string;
-  @Input() default:string;
+	@Input() src: string;
+	default: string = "assets/images/other-assets/profile.png";
 
-  updateUrl() {
-    this.src = this.default;
-  }
+	public updateUrl() {
+		return this.default;
+	}
+	public checkPath(src) {
+		return src ? src : this.default;
+	}
 }
